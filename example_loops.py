1ea0da75347bba597831addf0b60349b529c13f0
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-03-14 23:15:25 +0800

    Add is_power_of_two()

--- example_loops.py
+++ example_loops.py
@@ -14,3 +14,17 @@ def print_prime_factors(number):
 		return "Done"
 
 print_prime_factors(100)
+
+def is_power_of_two(n):
+	# Check if the number can be divided by two without a remainder
+	while n != 0 and n % 2 == 0:
+		n = n / 2
+	# If after dividing by two the number is 1, it's a power of two
+	if n == 1:
+		return True
+	return False
+
+print(is_power_of_two(0)) # Output: False
+print(is_power_of_two(1)) # Output: True
+print(is_power_of_two(8)) # Output: True
+print(is_power_of_two(9)) # Output: False
