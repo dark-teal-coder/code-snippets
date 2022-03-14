f3c42312124d0a9c42785bc178f53fe42db4efe2
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-03-14 23:35:17 +0800

    Add sum_divisors()

--- example_loops.py
+++ example_loops.py
@@ -28,3 +28,23 @@ print(is_power_of_two(0)) # Output: False
 print(is_power_of_two(1)) # Output: True
 print(is_power_of_two(8)) # Output: True
 print(is_power_of_two(9)) # Output: False
+
+
+def sum_divisors(n):
+  sum = 0
+  # Return the sum of all divisors of n, not including n
+  divisor = 1 
+  while divisor < n: 
+    if n % divisor == 0: 
+      sum += divisor
+    divisor += 1
+  return sum
+
+print(sum_divisors(0))
+# 0
+print(sum_divisors(3)) # Should sum of 1
+# 1
+print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
+# 55
+print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
+# 114
