2411b91ac0dd51e75c3dcb60b23db031704ac61f
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-03-20 15:55:44 +0800

    Add Home Team VS. Away Team

--- example_loops.py
+++ example_loops.py
@@ -64,8 +64,17 @@ print(factorial(5)) # should return 120
 
 
 
-## Domino Tiles
+## Nested Loops
+
+### Domino Tiles
 for left in range(7): 
 	for right in range(left, 7): 
 		print("[" + str(left) + "|" + str(right) + "]", end=" ")
 	print()
+
+### Home Team VS. Away Team 
+teams = [ 'Dragons', 'Wolves', 'Pandas', 'Unicorns']
+for home_team in teams:
+    for away_team in teams:
+		if home_team != away_team:
+			print(home_team + " vs. " + away_team)
