ab3c9d5f4d5679ec9e2efa27dd4f1eba55985de1
Preesa Saewong <ree.wong.career@gmail.com>
2022-04-29 19:56:28 +0800

    Change to Real Python website

--- start-hsbc-virtual-connect.py
+++ start-hsbc-virtual-connect.py
@@ -2,23 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time 
 
-passcode = input("Please input passcode from SecurID app: ")
-
-driver = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\chromedriver')
+driver = webdriver.Chrome(executable_path=r"C:\Program Files (x86)\chromedriver")
 driver.maximize_window()
 driver.implicitly_wait(10)
 
 ## Go to the website
-driver.get("https://tko.exconnect.hsbc.com.hk/logon/LogonPoint/tmindex.html")
-
-login_box = driver.find_element_by_id("login")
-login_box.send_keys('45244360')
-
-password_box = driver.find_element_by_id('passwd1')
-password_box.send_keys('Pr92180709')
-
-passcode_box = driver.find_element_by_id('passwd')
-passcode_box.send_keys(passcode)
+driver.get("https://realpython.com/")
+## Find the login button and click it
+login_button = driver.find_element_by_xpath('//*[@id="navbarSupportedContent"]/ul[2]/li[2]/a')
+login_button.submit()
 
 # login_box.send_keys(Keys.ENTER)
 # login_box.submit()
