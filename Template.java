f4c664c6ea2011427cd9fae5f8cb9eab99cf2c0b
Preesa Saewong <ree.wong.career@gmail.com>
2022-05-13 00:47:25 +0800

    Add a Java template

---
+++ Template.java
@@ -0,0 +1,8 @@
+// Every Java application begins with a class name and it must match the filename. 
+// Class names should be in mixed case with the 1st letter of each word capitalized.
+
+public class Template{
+	public static void main(String[] args) {
+		System.out.println("Hi, I'm a template!");
+	}
+}
